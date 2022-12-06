f1233faacb0ff1db58a28a163834177a9e874b29
iep-coder <37346685+iep-coder@users.noreply.github.com>
2022-12-06 15:21:11 -0800

    added EPA benthic invert traits database file and started a script to explore it

---
+++ BenthicInverts/benthic_inverts_traits_epa.R
@@ -0,0 +1,18 @@
+#Special Studies Drought Synthesis
+#Benthic Invertebrates
+#EPA Freshwater Biological Traits
+
+#Nick Rasmussen, nicholas.rasmussen@water.ca.gov
+
+#Freshwater Biological Traits Database 
+#https://www.epa.gov/risk/freshwater-biological-traits-database-traits#:~:text=The%20Freshwater%20Biological%20Traits%20Database,on%20river%20and%20stream%20ecosystems
+
+# Load required packages -----------------
+library(tidyverse) #suite of data science tools
+library(readxl) #read excel files
+
+#read in data
+epa_traits <- read.
+
+
+
