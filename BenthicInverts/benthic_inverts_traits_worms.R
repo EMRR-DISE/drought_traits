323bbe3625edcb212d37f5b01be660082a903044
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-02-03 10:30:27 -0800

    finished developing code for example of pulling trait data from worms

--- BenthicInverts/benthic_inverts_traits_worms.R
+++ BenthicInverts/benthic_inverts_traits_worms.R
@@ -9,25 +9,67 @@
 #filter dataset to just desired trait
 #do a bunch of unnesting of this poorly structured dataset
 
-#filter to just size "measurementType"
-
 # Load required packages -----------------
-library(worrms)
+library(tidyverse) #suite of data science tools
+library(worrms) #package for pulling data from WoRMS
+library(janitor) #misc functions for cleaning data
 
-# Examples from package documentation ---------
+# read in data ---------------
 
-ex1 <- wm_attr_data(id = 1040874)
-ex2 <- wm_attr_data_(id = c(127160, 126436))
+#just need the taxonomy info for our target taxa
+target_tax <- read_csv("./BenthicInverts/benthic_taxonomy_common5_2023-02-02.csv")
 
-#unnesting
-test <- ex1 %>% 
+# Example ---------
+#first two taxa in our data set
+
+#vector of aphia ids
+b_aphia <- c(992855,1040874)
+
+#run aphia ids through the function that grabs the trait data from worms
+w_traits <- wm_attr_data_(id = b_aphia) 
+
+#explore the type of trait data in resulting output dataframe
+w_traits_dist <- w_traits %>% 
+  #count number of records (ie, rows) for all distinct combos of the measurement types and associated ID numbers
+  group_by(measurementTypeID,measurementType) %>% 
+  summarize(records = n())
+#four types of traits, most records for body size
+
+#body size: unnesting
+w_bsize <- w_traits %>% 
+  #need to subset to just the body size records first
+  #otherwise the content of columns for trait data is a mix of unrelated info
   filter(measurementTypeID==15) %>% 
-  unnest(cols = children, names_repair = "universal") %>% 
+  #first unnesting; need to do name repair because of duplication of column names
+  #this unnested the size measurement units (mm)
+  unnest(cols = children, names_repair = "universal") %>%  
+  #unnest again to get the type of body size (min vs max)
   unnest(cols = children) %>% 
+  #unnest again to get body width vs body length category
   unnest(cols = children, names_repair = "universal") %>% 
-  unnest(children)
+  #unnest again to get columns indicating these are traits for adults
+  unnest(children) %>% 
+  #clean up format of column names
+  clean_names() %>% 
+  glimpse()
 
-  
+#body size: format records of max adult body length
+w_bsize_ft <- w_bsize %>% 
+  #only keep needed columns and rename them
+  #lots of repetition of columns once unnested
+  select(
+    aphia_id = aphia_id_2
+    ,trait_value=measurement_value_5
+    ,units = measurement_value_14
+    ,life_stage = measurement_value
+    ,body_dimension = measurement_value_32
+    ,body_measure_type = measurement_value_23
+    ,quality_status=qualitystatus_8
+    ,citation=reference_7
+      ) %>% 
+  #just keep adult max body length
+  filter(life_stage=="adult" & body_dimension=="length" & body_measure_type=="maximum") %>% 
+  glimpse()
   
   
   
