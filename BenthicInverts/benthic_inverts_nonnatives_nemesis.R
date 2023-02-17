933d7bbd80dbe1aee9ad72e631ad6cbc7997dd08
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-02-17 14:56:42 -0800

    completed draft of script that categorizes benthic invert taxa as native or non-native and exports data as csv

--- BenthicInverts/benthic_inverts_nonnatives_nemesis.R
+++ BenthicInverts/benthic_inverts_nonnatives_nemesis.R
@@ -6,6 +6,16 @@
 
 #Nick Rasmussen, nicholas.rasmussen@water.ca.gov
 
+#To do list-----------------
+
+#Changes to make upstream of this script in taxonomy script: 
+#Sinelobus sp. A to Sinelobus stanfordi
+#Synidotea laevidorsalis to S. laticauda
+
+#should add estimated year of introduction for non-natives
+#useful because some of the important taxa arrived during the time series we're analyzing
+#also need to pair this trait data with literature references (largely Cohen and Carlton 1995)
+
 # Load required packages -----------------
 
 library(janitor) #used to quickly clean up column names
@@ -23,7 +33,7 @@ benthic_aliens <- read_excel("./BenthicInverts/IEP_BenthicOrganismList2021_w_Int
 focal_taxa <- read_csv("./BenthicInverts/benthic_taxonomy_common5_2023-02-02.csv") %>% 
   glimpse()
 
-# Format data ---------------------
+# Format data in prep for combining ---------------------
 
 #focal taxa data
 focal_taxa_format <- focal_taxa %>% 
@@ -68,21 +78,40 @@ focal_taxa_comp <- aliens_focal %>%
          ) %>% 
   arrange(taxon_match,comments)
 
-#create subset of taxa with taxonomic discrepancies or other issues to send to Betsy
-
+#create subset of taxa with taxonomic discrepancies or other issues to examine
 focal_taxa_issue <- focal_taxa_comp %>% 
   filter(taxon_match==0 | !is.na(comments))
 #most discrepancies are simply from me updating names using worms so they're fine as is
 #Sharon indicates that Laonome calida should be Laonome cf. calida, so use literature data with caution
 #she also labels as Sinelobus sp. A as Sinelobus cf. stanfordi, which is better than just genus level but again use species data with caution
-#she labeled Manayunkia speciosa as cryptogenic; maybe just label as native for simplicity
+#she labeled Manayunkia speciosa as cryptogenic; maybe just label as invasive for simplicity (following Cohen and Carlton 1995)
 #she labeled Synidotea laevidorsalis as S. laticauda. both are valid names
 #unclear if ours is S. laevidorsalis or S. laticauda but NEMESIS suggests probably S. laticauda so let's use that
 
-#so changes to make:
+#changes to make here: 
+#Manayunkia speciosa change from cryptogenic to non-native
+#assume Sinelobus sp. A is Sinelobus stanfordi and change from Introduced? to Introduced
+
+#changes to make upstream in taxonomy script: 
 #Sinelobus sp. A to Sinelobus stanfordi
-#Manayunkia speciosa change from cryptogenic to native
 #Synidotea laevidorsalis to S. laticauda
 
+# Format the data set filtered to just my target taxa ---------------
+#NOTE: assuming any taxon not specificly IDed as non-native is native
+#clearly some of these taxa left as native could be non-native, particularly the ones not IDed to species
 
+aliens_focal_format <- aliens_focal %>% 
+  select(organism_code,origin) %>% 
+  mutate(
+    #change one case of Introduced? and Cryptogenic to Introduced (following Cohen and Carlton 1995 which might be outdated)
+    #fill NAs with Native
+    native = case_when((origin == "Introduced?" | origin == "Cryptogenic" | origin == "Introduced") ~ "0"
+                        , is.na(origin) ~ "1")
+                        #,TRUE~origin)
+    ) %>% 
+  select(organism_code,native) %>% 
+  arrange(organism_code) %>% 
+  glimpse()
 
+#export the complete file
+#write_csv(aliens_focal_format,"./BenthicInverts/bentic_inverts_trait_origin.csv")
