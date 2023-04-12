c9f8607ff6a912a0a026dc7178a86e3d38af8450
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-04-12 16:32:02 -0700

    working on scraping multiple webpages at once

--- BenthicInverts/calnemo/benthic_nemesis_traits.R
+++ BenthicInverts/calnemo/benthic_nemesis_traits.R
@@ -4,8 +4,9 @@
 #specific table but it selects all the tables (which I don't want)
 
 #packages-----------
-library(tidyverse)
-library(rvest)
+library(tidyverse) #suite of data science tools
+library(rvest) #scraping info from the internet
+library(purrr) #map functions for scraping multiple webpages at once
 
 #read in data----------
 
@@ -23,6 +24,9 @@ nemesis_links <- read_csv("./BenthicInverts/calnemo/benthic_nemesis_links.csv")
 
 #step by step how to scrape html table
 #https://dcl-wrangle.stanford.edu/rvest.html
+
+#webscraping with R manual
+#https://webscraping-tures.github.io/rvest3.html
 
 #single webpage: scrape table by specifying which table number on the webpage---------------
 #I think the number and order of tables on each webpage is standardized
@@ -43,6 +47,7 @@ df <- url %>%
   #creates list of 6 tibbles because there isn't just one table
   html_table() %>%   
   #just keep the third table of the webpage
+  #[3] would format it as a list, [[3]] formats it as a df (what we want)
   .[[3]] %>% 
   #create better column headers
   rename(trait = X1
@@ -80,12 +85,41 @@ test_table <- url %>%
   
 #scrape table from multiple webpages----------------------
 
+#go to each URL
+#read the html
+#turn the tables into tibbles
+#keep the third table only
+#format the column names
+#add the URL as a column
+#combine all tables into one table
+#join with df with organism codes and taxon names
+
+
 #start with simple example with only two URLs
 url2 <-  c("https://invasions.si.edu/nemesis/species_summary/81746","https://invasions.si.edu/nemesis/species_summary/-82")
 
 #create vector of all URLs
+#note that all URLs are the same except for the webpage specific number at the end
+# prefix = https://invasions.si.edu/nemesis/species_summary/
 url_all <- nemesis_links %>% 
   pull(link)
+
+#try to scrape the specific table from all webpages
+#eventually replace ulr2 with url_all
+tables_all <- url2 %>% 
+  #read the html from all the webpages
+  map(read_html) %>% 
+  #grab just the tables (multiple per webpage)
+  map(html_elements,css="table") %>% 
+  #convert html to tibbles
+  map(html_table)  
+  #just keep the third table 
+  map(.[[3]]) #this doesn't work
+
+#example
+pages %>% 
+  map(html_node, css = "a.navbar-brand") %>% 
+  map_chr(html_text)
 
 #function for scraping tables from multiple webpages
 #is it possible to just grab the third table from each webpage instead of grabbing all tables?
@@ -101,7 +135,6 @@ html_table (fill=T) %>% .[[3]]
 #run the function on the set of webpage URLs
 my_scraped_data <- lapply(url2, read_my_urls)
 
-#need to go into the list of tables for each webpage and grab the third table
 
 
 
