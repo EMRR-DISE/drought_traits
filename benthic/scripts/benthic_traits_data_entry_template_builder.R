497ae33291f6e96a1cfeda99e7f4e8ee327ca3e7
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-06-16 10:24:05 -0700

    added a column for origin to final data frame

--- benthic/scripts/benthic_traits_data_entry_template_builder.R
+++ benthic/scripts/benthic_traits_data_entry_template_builder.R
@@ -15,10 +15,16 @@ library(janitor) #misc functions for cleaning data
 #read in relative abundance data file
 abundance <- read_csv("./benthic/data_output/benthic_relative_abundances.csv") 
 
-#filter the abundance data frame to only keep the 14 most abundant
+origin <-read_csv("./benthic/data_output/traits/benthic_traits_nemesis_origin_taxa64.csv")
+
+#filter the abundance data frame to only keep the 14 most abundant and just needed columns
 abund14 <- abundance %>% 
   filter(rank_no<15) %>% 
   select(organism_code,species_name,rank_no)
+
+#reduce size of origin dataframe to just needed columns
+origin_sub <- origin %>% 
+  select(organism_code,native)
 
 #make the other needed data frames
 
@@ -34,13 +40,18 @@ assign <- data.frame(
 
 #combine data frames
 
+#all combinations of taxa and traits
 combo <- crossing(abund14,trait) %>% 
   arrange(rank_no,organism_code)
 
+#add the assignments
 combo2 <- data.frame(cbind(combo,assign))
 
+#add the origin for each
+combo3 <- left_join(combo2,origin_sub)
+
 #write file
-#write_csv(combo2,"./benthic/data_output/benthic_traits_data_entry_rows.csv")
+#write_csv(combo3,"./benthic/data_output/benthic_traits_data_entry_rows.csv")
 
 
 
