d20b8ceece73cfe32aa7037c8a98b0e40c277110
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-08-22 14:34:22 -0700

    added code to run the RQL, fourth corner analysis, and combo analysis on first draft of data

--- benthic/scripts/benthic_fourthcorner_rql.R
+++ benthic/scripts/benthic_fourthcorner_rql.R
@@ -18,13 +18,31 @@ abund <- read_csv("./benthic/data_output/benthic_table_l.csv") %>%
   glimpse()
 
 #r df
-temp <- read_csv("fish traits/fish_data/drought_variables.csv") #just took from fish for now
-temp[55,10] <- mean(temp$Phos, na.rm = T) # replace NA w mean Phos for 2021
+temp <- read_csv("fish traits/fish_data/drought_variables.csv") %>%  #just took from fish for now
+  #clean up column names
+  clean_names() %>% 
+  glimpse()
+
+temp[55,10] <- mean(temp$phos, na.rm = T) # replace NA w mean Phos for 2021
+
 envn <- temp %>%
   filter(year >= "1981" & year != "2022" & year!="2004" & year!="2005")%>% 
-  select(c(year, drought_year, water_year_sac, inflow_annual_cfs:Temperature)) %>% 
-  mutate(water_year_sac = as_factor(water_year_sac)) %>% 
-  relocate(water_year_sac, .before = drought_year) %>% 
+  #let's just start with a subset of variables, focusing on continuous ones (ie, no factors)
+  #also let's drop flow because highly correlated with salinity
+  select(
+    year
+    #, drought_year
+    #, water_year_sac
+    #, inflow_annual_cfs
+    , nitrate
+    , ammonia
+    , phos
+    , salinity
+    , secchi
+    , temperature
+    ) %>% 
+  #mutate(water_year_sac = as_factor(water_year_sac)) %>% 
+  #relocate(water_year_sac, .before = drought_year) %>% 
   column_to_rownames(var = "year") %>% 
   glimpse()
 
@@ -38,8 +56,8 @@ trait <- read_csv("./benthic/data_output/traits/benthic_table_q.csv") %>%
 
 #check dimensions
 dim(abund) #39 x 61 correspondance
-dim(envn) #39 x 9 all quantitative -- PCA
-dim(trait) #61 x 3 hillsmith, quantitative and factor
+dim(envn) #39 x 6 all quantitative -- PCA
+dim(trait) #61 x 2 hillsmith, quantitative and factor
 
 #correspondance for abundance data
 afcL.benthic <- 
@@ -47,13 +65,22 @@ afcL.benthic <-
 summary(afcL.benthic) 
 #total inertia 1.107
 
-#hill-smith for environmental data
+#start with PCA for environmental data
 acpR.benthic <- 
-  dudi.hillsmith(envn, 
+  dudi.pca(envn, 
                  row.w = afcL.benthic$lw,
                  scannf = F,
-                 nf = 2)
+                 nf = 2 #leave this out for now to see all eigenvalues
+           )
 score(acpR.benthic)
+
+#hill-smith for environmental data
+#acpR.benthic <- 
+#  dudi.hillsmith(envn, 
+#                  row.w = afcL.benthic$lw,
+#                  scannf = F,
+#                  nf = 2)
+# score(acpR.benthic)
 
 #hill-smith again for trait data
 acpQ.benthic <- # 'Q' table of traits by species
@@ -62,6 +89,98 @@ acpQ.benthic <- # 'Q' table of traits by species
     row.w = afcL.benthic$cw,
     scannf = F)
 score(acpQ.benthic)
+
+#RQL analysis-------
+
+#build model
+rlq.benthic <- rlq(acpR.benthic, afcL.benthic, acpQ.benthic,
+                 scannf = FALSE)
+
+#plot results
+plot(rlq.benthic)
+
+#look at results summary
+summary(rlq.benthic)
+
+#plot subset of graphs
+par(mfrow = c(1, 3))
+s.arrow(rlq.benthic$l1)
+s.arrow(rlq.benthic$c1)
+s.label(rlq.benthic$lQ, boxes = FALSE)
+
+
+#Fourth corner analysis--------------
+
+#build model
+#this is version without adjustment of pvalues for multiple comparisons
+nrepet <- 49999 
+four.comb.benthic <- fourthcorner(envn, abund,
+                                trait, modeltype = 6, p.adjust.method.G = "none",
+                                p.adjust.method.D = "none", nrepet = nrepet)
+
+#plot results
+par(mfrow = c(1, 1))
+plot(four.comb.benthic, alpha = 0.05, stat = "D2")
+#no significant relationships
+
+#rerun model with adjustment of pvalues
+#not much point though given that analysis w/o pvalue adjustment had no sign. comparisons
+four.comb.benthic_padj <- fourthcorner(envn, abund,
+                                  trait, modeltype = 6, p.adjust.method.G = "fdr",
+                                  p.adjust.method.D = "fdr", nrepet = nrepet)
+plot(four.comb.benthic_padj, alpha = 0.05, stat = "D2")
+
+#combined RQL and Fourth Corner---------------
+
+testrlq.benthic <- randtest(rlq.benthic, modeltype = 6, nrepet = nrepet)
+testrlq.benthic
+#Model 2 sign. but Model 4 not (p = 0.1)
+
+plot(testrlq.benthic)
+
+#The total inertia of RLQ analysis is equal to the SRLQ multivariate statistic defined in Dray and
+#Legendre (2008). This statistic is returned by the fourthcorner2 function
+Srlq <- fourthcorner2(envn,abund,trait,
+                      modeltype = 6, p.adjust.method.G = "fdr", nrepet = nrepet)
+Srlq$trRLQ 
+
+#biplot
+#plot should be devoid of relationship lines
+plot(four.comb.benthic_padj, x.rlq = rlq.benthic, alpha = 0.05,
+     stat = "D2", type = "biplot")
+#yep just shows the traits and env predictors
+
+#Another approach is provided by the fourthcorner.rlq function and consists in testing directly the
+#links between RLQ axes and traits (typetest="Q.axes") or environmental variables (typetest="R.axes").
+testQaxes.comb.benthic <- fourthcorner.rlq(rlq.benthic, modeltype = 6,
+                                         typetest = "Q.axes", nrepet = nrepet, p.adjust.method.G = "fdr",
+                                         p.adjust.method.D = "fdr")
+testRaxes.comb.benthic <- fourthcorner.rlq(rlq.benthic, modeltype = 6,
+                                         typetest = "R.axes", nrepet = nrepet, p.adjust.method.G = "fdr",
+                                         p.adjust.method.D = "fdr")
+print(testQaxes.comb.benthic, stat = "D")
+#no significant pvalues
+#AxcR1 / body_size is close before adjustment (p = 0.07)
+
+print(testRaxes.comb.benthic, stat = "D")
+#none are significant but four are close
+
+#Results can be represented using a table with colors indicating significance :
+par(mfrow = c(1, 2))
+plot(testQaxes.comb.benthic, alpha = 0.05, type = "table",
+     stat = "D2")
+plot(testRaxes.comb.benthic, alpha = 0.05, type = "table",
+     stat = "D2")
+#no significant relationships to show
+
+# #Significance with axes can also be reported on the factorial map of RLQ analysis. Here, significant
+# associations with the first axis are represented in blue, with the second axis in orange, with both axes in
+# green (variables with no significant association are in black):
+  par(mfrow = c(1, 2))
+plot(testQaxes.comb.benthic, alpha = 0.05, type = "biplot",
+     stat = "D2", col = c("black", "blue", "orange", "green"))
+plot(testRaxes.comb.benthic, alpha = 0.05, type = "biplot",
+     stat = "D2", col = c("black", "blue", "orange", "green"))
 
 # Format data for analysis ----------
 #maybe should look at distribution of abundances
