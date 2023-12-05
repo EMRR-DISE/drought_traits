57ab9cb23b72995257e4c254f1e403e75151f276
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-12-05 13:25:01 -0800

    finished assembling trait table for dominant benthic inverts

--- benthic/scripts/benthic_traits_integrated_dominant_taxa.R
+++ benthic/scripts/benthic_traits_integrated_dominant_taxa.R
@@ -5,9 +5,9 @@
 #Combine trait data from the various sources
 
 #to do list
-#probably need to choose which trait value to use for a small subset of traits where there are multiple for a taxon
-#check to see how prevalent that is while formatting data set
-#are all units standardized within trait group?
+#salinity is a mix of PSU and PPT; I think these are pretty similar but maybe consider standardizing these if possible
+#maybe need to convert categorical traits to integers instead of letters for analysis; not sure
+#for final analysis, make sure traits like salinity and thermal tolerance are as comparable as possible
 
 #traits included:
 #body length
@@ -49,6 +49,9 @@ traits_lit_dom <- traits_lit %>%
   #filter out non-dominant taxa
   filter(!(organism_code %in% nondom))
 
+#create df with dominant organism codes and names
+traits_lit_dom_taxa <- traits_lit_dom %>% 
+  distinct(organism_code,target_taxon_name)
 
 #look at list of unique trait groups
 unique(traits_lit_dom$trait_group) #13 trait groups
@@ -56,18 +59,15 @@ unique(traits_lit_dom$trait_group) #13 trait groups
 #which taxa have multiple values for a trait group?
 traits_lit_group_sum <- traits_lit_dom %>% 
   group_by(organism_code,trait_group) %>% 
-  summarise(n = sum(!is.na(trait_value)),.groups ="drop") %>% 
-  arrange(-n)
+  summarise(n = as.numeric(sum(!is.na(trait_value)),.groups ="drop")) %>% 
+  arrange(-n) %>% 
+  glimpse()
 #four instances of multiple taxon x trait combos
 #use trait_value_rank == 1 to reduce to just the best trait value
 
-#still not doing quite what I want
-traits_lit_group_complete <-as.data.frame(table(traits_lit_dom$organism_code,traits_lit_dom$trait_group)) %>% 
-  group_by(Var2) %>% 
-  count()
-
 traits_lit_format <- traits_lit_dom %>% 
   #filter out the trait values that aren't the best one in cases of multiple trait values
+  #this also drops the missing trait data rows because those are NA instead of 1
   filter(trait_value_rank==1) %>% 
   #just keep the needed columns
   select(
@@ -76,13 +76,48 @@ traits_lit_format <- traits_lit_dom %>%
     ,trait_value
     ,trait_unit
   ) %>% 
-  #drop thermal_tolerance for now because incomplete
-  filter(trait_group!="thermal_tolerance")
+  glimpse()
+
+#which traits have values for all taxa?
+traits_lit_group_miss <- traits_lit_format %>% 
+  group_by(trait_group) %>% 
+  summarise(n = sum(!is.na(trait_value)),.groups = 'drop') %>% 
+  arrange(-n)
+#complete (n = 14): armoring, dispersal, habit, trophic_habit, voltinism, reproduction; salinity_tolerance_cont
+#close: thermal_tolerance (11); drop this one for now
+#drop the other trait groups
+
+#create list of trait groups to keep
+trait_group_complete <- traits_lit_group_miss %>% 
+  filter(n==14) %>% 
+  pull(trait_group)
+
+#filter out trait groups with missing data and convert long to wide
+traits_lit_group_wide <- traits_lit_format %>% 
+  #just keep complete trait groups
+  filter(trait_group %in% trait_group_complete) %>% 
+  arrange(trait_group,trait_value) %>% 
+  #drop unneeded units column
+  select(-trait_unit)  %>% 
+  pivot_wider(names_from = trait_group, values_from = trait_value)
+  
 
 #format body size and origin data set----------------------
 #filter to just the 14 dominant taxa
 #probably just use a left_join() to the formatted lit traits data set 
 
+traits_dom_all <- left_join(traits_lit_group_wide,size_origin_all) %>%   
+  #reorder columns
+  select(organism_code
+         ,name = target_taxon_name
+         ,armoring:voltinism
+         ,body_size
+         ,native
+         ) %>% 
+  arrange(organism_code) %>% 
+  glimpse()
 
+#write file with trait matrix
+#write_csv(traits_dom_all,"./benthic/data_output/traits/benthic_dom_table_q.csv")
 
 
