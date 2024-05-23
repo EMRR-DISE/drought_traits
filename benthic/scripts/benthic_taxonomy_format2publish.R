3168c4d141267e164e0463f48c327e9207cce0c8
iep-coder <37346685+iep-coder@users.noreply.github.com>
2024-05-23 12:56:29 -0700

    make sure the body size table and taxonomy table will match

--- benthic/scripts/benthic_taxonomy_format2publish.R
+++ benthic/scripts/benthic_taxonomy_format2publish.R
@@ -18,6 +18,14 @@ taxonomy <- read_csv("./benthic/data_output/benthic_common5_taxonomy_2023-03-27.
 #origin
 origin <- read_csv("./benthic/data_output/traits/benthic_traits_nemesis_origin_taxa64.csv")
 
+#dominant taxa traits
+traitd <- read_csv("./benthic/data_output/traits/benthic_traits_nemesis_origin_taxa64.csv")
+
+#non-rare traits
+traitnr <- read_csv("./benthic/data_output/traits/benthic_nonrare_size_edi_2024-05-17.csv") %>% 
+  glimpse()
+
+
 #reduce origin df to just the needed columns
 originf <- origin %>% 
   select(organism_code,native)
@@ -33,13 +41,30 @@ final <- taxor %>%
   #drop the taxa that aren't genus or species
   filter(rank == "Species"| rank=="Genus") %>% 
   select(organism_code
-         ,taxon
-         ,aphia_id
+         #,target_taxon_name = taxon
+         #,target_taxon_level = rank
          ,source
-         ,rank:native
-    )
+         ,aphia_id
+         ,kingdom:native
+    ) %>% 
+  glimpse()
 
-#write_csv(final,"./benthic/data_output/benthic_nonrare_taxonomy_origin_EDI.csv")
+# Make sure the trait and taxonomy tables will join--------
+
+#combine taxonomy and body size
+tt1 <- left_join(traitnr,final) %>% 
+  glimpse()
+#these two tables matched perfectly by organism_code
+#will use the size trait table version of target_taxon_name and target_taxon_level
+
+final_formatted <- tt1 %>% 
+  select(organism_code:target_taxon_level
+         ,source:native
+         ) %>% 
+  glimpse()
+
+
+#write_csv(final_formatted,"./benthic/data_output/edi/benthic_nonrare_taxonomy_origin_edi.csv")
 
 
 
